30923bb29ce69633dddab09586d52ba8c779e422
munjoonteo <teomunjoon101@gmail.com>
2020-08-16 21:53:24 +1000

    player names can be stored and retrieved from local storage

--- screens/PointsTable.tsx
+++ screens/PointsTable.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity, AsyncStorage } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
-export default function HomeScreen() {
+export default function PointsTable() {
+  const [savedNames, setSavedNames] = React.useState([]);
+
+  React.useEffect(() => {
+    AsyncStorage.getItem("savedNames").then((data) => {
+      if (data !== null) {
+        setSavedNames(JSON.parse(data));
+      }
+    });
+  }, savedNames);
+
+  console.log(savedNames);
+
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <View style={styles.list}>
+        <Text>{savedNames[0]}</Text>
+        <Text>{savedNames[1]}</Text>
+        <Text>{savedNames[2]}</Text>
+        <Text>{savedNames[3]}</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +31,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
   },
   title: {
@@ -25,5 +41,12 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  list: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: "teal",
+    borderBottomWidth: 1,
+    paddingHorizontal: 20,
   },
 });
--- screens/Setup.tsx
+++ screens/Setup.tsx
@@ -1,14 +1,69 @@
 import * as React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  AsyncStorage,
+} from "react-native";
 
 import { Text, View } from "../components/Themed";
 
-export default function HomeScreen({ navigation }: any) {
+export default function Setup({ navigation }: any) {
+  const [east, setEast] = React.useState("");
+  const [south, setSouth] = React.useState("");
+  const [west, setWest] = React.useState("");
+  const [north, setNorth] = React.useState("");
+
+  const saveNames = async (
+    navigation: any,
+    east: string,
+    south: string,
+    west: string,
+    north: string
+  ) => {
+    if (east === "" || south === "" || west === "" || north === "") {
+      return;
+    }
+    let savedNames = [east, south, west, north];
+    try {
+      await AsyncStorage.setItem("savedNames", JSON.stringify(savedNames));
+      navigation.navigate("PointsTableScreen");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Setup</Text>
+      <View>
+        <TextInput
+          style={styles.setName}
+          onChangeText={(text) => setNorth(text)}
+          value={north}
+        />
+      </View>
+      <View>
+        <TextInput
+          style={styles.setName}
+          onChangeText={(text) => setWest(text)}
+          value={west}
+        />
+        <TextInput
+          style={styles.setName}
+          onChangeText={(text) => setEast(text)}
+          value={east}
+        />
+      </View>
+      <View>
+        <TextInput
+          style={styles.setName}
+          onChangeText={(text) => setSouth(text)}
+          value={south}
+        />
+      </View>
       <TouchableOpacity
-        onPress={() => navigation.navigate("PointsTableScreen")}
+        onPress={() => saveNames(navigation, east, south, west, north)}
       >
         <Text>Play!</Text>
       </TouchableOpacity>
@@ -30,5 +85,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  setName: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "black",
   },
 });
